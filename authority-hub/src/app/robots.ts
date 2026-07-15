9befd47d06610a27f5b85c92750555f836d2708e
igor vepretski <149789877+vepretski@users.noreply.github.com>
2026-07-15 22:59:57 +0300

    feat(authority-hub): add robots policy

---
+++ authority-hub/src/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/_next/']
+    },
+    sitemap: 'https://7ya.io/sitemap.xml',
+    host: 'https://7ya.io'
+  };
+}
