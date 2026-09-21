d72e8fc79a50b84fe26d26af4f8e0a4a3ced9ae0
igor vepretski <149789877+vepretski@users.noreply.github.com>
2026-09-21 00:29:07 -0700

    Fix chat release gate and invalidate stale cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '7ya-shell-20260920-speak1';
+const CACHE_VERSION = '7ya-shell-20260920-speak2';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = '/';
